e0afd4c934c7754a211f795e7329b8457e41759d
Piero Cassano <danilope20050605@gmail.com>
2026-03-31 20:51:43 -0500

    Update cineGet.js

--- Cinestar_Entregable/js/firebase/cineGet.js
+++ Cinestar_Entregable/js/firebase/cineGet.js
@@ -1,14 +1,6 @@
 import { db } from './firebase.js';
-import { 
-    collection, 
-    getDocs, 
-    query, 
-    where, 
-    doc, 
-    getDoc 
-} from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
+import { collection, getDocs, query, where, doc, getDoc } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
 
-// 1. Obtener todos los cines (para cines.html)
 export const getCines = async () => {
     try {
         const cinesCol = collection(db, "cines");
@@ -20,7 +12,6 @@ export const getCines = async () => {
     }
 };
 
-// 2. Obtener el detalle de un cine por su ID (para cine.html)
 export const getCineById = async (id) => {
     try {
         const docRef = doc(db, "cines", id);
@@ -31,10 +22,8 @@ export const getCineById = async (id) => {
     }
 };
 
-// 3. Obtener las TARIFAS de un cine (Usando la colección cinetarifas de tu imagen)
 export const getTarifasByCine = async (idCine) => {
     try {
-        // Importante: idCine debe coincidir con el valor en Firestore (ej: "2")
         const q = query(collection(db, "cinetarifas"), where("idCine", "==", idCine));
         const querySnapshot = await getDocs(q);
         return querySnapshot;
