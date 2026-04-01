851136974501e58a91721a48f050f9c974feb769
Piero Cassano <danilope20050605@gmail.com>
2026-03-31 20:52:07 -0500

    Update firebase.js

--- Cinestar_Entregable/js/firebase/firebase.js
+++ Cinestar_Entregable/js/firebase/firebase.js
@@ -1,12 +1,10 @@
-// 1. Importamos lo necesario de la versión 12.11.0
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
 
-// 2. Tu nueva configuración
 const firebaseConfig = {
     apiKey: "AIzaSyBSfVTJUs3-ziPOwgyiFx1lJgrUCxsL1Js",
     authDomain: "cinestar-a8730.firebaseapp.com",
-    databaseURL: "https://cinestar-a8730-default-rtdb.firebaseio.com", // Esto es para Realtime DB, pero no estorba
+    databaseURL: "https://cinestar-a8730-default-rtdb.firebaseio.com",
     projectId: "cinestar-a8730",
     storageBucket: "cinestar-a8730.firebasestorage.app",
     messagingSenderId: "1039213486545",
@@ -14,8 +12,6 @@ const firebaseConfig = {
     measurementId: "G-PXWZ1MXBHS"
 };
 
-// 3. Inicializamos Firebase
 const app = initializeApp(firebaseConfig);
 
-// 4. Exportamos la base de datos Firestore (¡Súper importante!)
 export const db = getFirestore(app);
